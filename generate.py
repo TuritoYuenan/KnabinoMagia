e79d3e6fcaa850308645d4a028a4f964d4a735dc
TuritoYuenan <turitoyuenan@proton.me>
2022-08-25 17:01:20 +0700

    new mixed changes

--- generate.py
+++ generate.py
@@ -29,13 +29,13 @@ magical_girls = [
 	'tiffany-grey',
 ]
 
-#for i in countries:
-#	os.system(f'hugo new countries/{i}.md')
-#	time.sleep(1)
-#
-#for i in magical_girls:
-#	os.system(f'hugo new magical-girls/{i}.md')
-#	time.sleep(1)
+for i in countries:
+	os.system(f'hugo new countries/{i}.md')
+	time.sleep(1)
+
+for i in magical_girls:
+	os.system(f'hugo new magical-girls/{i}.md')
+	time.sleep(1)
 
 # Drop test subject
 os.system('hugo new magical-girls/test.md')
